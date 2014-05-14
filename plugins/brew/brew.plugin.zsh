b1b039b61a379a98573c11a1fc63cebc56748fca
Okura Masafumi <masafumi.o1988@gmail.com>
2014-05-15 05:11:15 +0900

    Add brew_check_install function

--- plugins/brew/brew.plugin.zsh
+++ plugins/brew/brew.plugin.zsh
@@ -1,1 +1,13 @@
 alias brews='brew list -1'
+
+function brew_check_install() {
+  for formula in `brew list`
+  do
+    if [ $formula = $1 ] ; then
+      echo 0
+      return
+    fi
+  done
+  echo 1
+  return
+}
